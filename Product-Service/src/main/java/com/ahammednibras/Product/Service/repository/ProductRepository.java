40aee53f706770e5439e1c1bf1b213339c0351b2
ahammednibras8 <ahammednibras@proton.me>
2025-04-09 22:08:57 +0530

    Initialize

--- Product-Service/src/main/java/com/ahammednibras/Product/Service/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ahammednibras.Product.Service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ahammednibras.Product.Service.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    // Spring Data JPA will implement this automatically
-}
